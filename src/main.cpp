2482dcd2001841266d0acc38c113359f0325b558
HamzaHindo <hm515047@gmail.com>
2025-09-23 14:53:01 +0300

    test fork

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,7 @@ int main() {
   filter.invertFilter("output/invert_image.jpg");
   filter.greyFilter("output/grey_image.jpg");
   filter.bwFilter("output/bw_image.jpg");
+
+  // MY new commit
   return 0;
 }
