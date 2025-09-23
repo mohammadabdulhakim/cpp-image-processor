6cf7cc891d3855a42556f06c5381e6705be48554
HamzaHindo <hm515047@gmail.com>
2025-09-23 14:44:20 +0300

    organize folder structure

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Filter_Class.h"
+#include "Image_Class.h"
+
+int main() {
+  Image img("img.jpg");
+
+  Filter filter(img);
+
+  filter.invertFilter("output/invert_image.jpg");
+  filter.greyFilter("output/grey_image.jpg");
+  filter.bwFilter("output/bw_image.jpg");
+  return 0;
+}
