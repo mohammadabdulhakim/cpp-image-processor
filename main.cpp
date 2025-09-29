fe06465083e39b0dc1b90c936d23ce7ae11379f6
HamzaHindo <hm515047@gmail.com>
2025-09-29 15:15:04 +0300

    add rotate filter

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "Image_Class.h"
 #include <stdexcept>
 #include "iostream"
-//using namespace std; // This load a lot of files into the project, use std:: better;
+#include <corecrt_math_defines.h>
 
 
 class Filter {
@@ -11,11 +11,12 @@ public:
   Filter() {}
   virtual void apply(std::string outputFilendame) = 0;
 };
-
 class GreyFilter : public Filter {
   Image image;
 public:
-  GreyFilter(Image image) : image(image) {}
+  GreyFilter(Image image) {
+    this->image = image;
+  }
   void apply(std::string outputFilename) override {
     try {
         for (int i = 0; i < image.width; i++) {
@@ -42,7 +43,6 @@ public:
     }
   }
 };
-
 class BwFilter : public Filter {
   Image image;
 public:
@@ -75,7 +75,6 @@ public:
     }
   }
 };
-
 class InvertFilter : public Filter {
   Image image;
 public:
@@ -97,7 +96,6 @@ public:
     }
   }
 };
-
 class MergeFilter : public Filter {
   Image base;
   Image overlay;
@@ -124,6 +122,57 @@ public:
     }
   }
 };
+class RotateFilter : public Filter {
+  Image image;
+  int angle;
+  int w, h;
+public:
+  RotateFilter(Image image, int angle) : image(image), angle(angle) {}
+  void apply(std::string outputFilename) override {
+        Image rotated_image;
+
+    switch (angle)
+    {
+    case 90:
+    case 270:
+        rotated_image = Image(image.height, image.width);
+        break;
+
+    case 180:
+        rotated_image = Image(image.width, image.height);
+        break;
+    }
+
+
+    double radian = angle * M_PI / 180.0;
+    double cos_Angle = cos(radian);
+    double sin_Angle = sin(radian);
+
+    int cx = image.width / 2;
+    int cy = image.height / 2;
+
+    int ncx = rotated_image.width / 2;
+    int ncy = rotated_image.height / 2;
+
+    for (int x = 0; x < rotated_image.width; x++)
+    {
+        for (int y = 0; y < rotated_image.height; y++)
+        {
+            int X = cx + (x - ncx) * cos_Angle + (y - ncy) * sin_Angle;
+            int Y = cy - (x - ncx) * sin_Angle + (y - ncy) * cos_Angle;
+
+            if (X >= 0 && X < image.width && Y >= 0 && Y < image.height)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    rotated_image(x, y, k) = image(X, Y, k);
+                }
+            }
+        }
+    }
+    rotated_image.saveImage(outputFolderPath + outputFilename);
+  }
+};
 
 Image loadImage () {
   std::cout << "Enter the image's name: "; std::string imgName; std::cin >> imgName;
@@ -133,7 +182,6 @@ Image loadImage () {
   return img;
 
 }
-
 
 int main() {
 
@@ -154,43 +202,50 @@ int main() {
   MergeFilter merge(toy1, toy2);  
   merge.apply("toy1,toy2.jpg");
 
-    std::string welcomeMsg = "\nWelcome to the ultimate image processor CPP app.";
-    std::cout << welcomeMsg << "\n";
-    std::cout << std::string(welcomeMsg.length()/5, ' ') << std::string(welcomeMsg.length()*3/5, '=') << std::string(welcomeMsg.length()/5, ' ') << "\n \n";
+  RotateFilter rotate_90(toy1, 90);
+  rotate_90.apply("rotated_90.jpg");
 
-    bool exited = false;
-    bool fileLoaded = false; // default false
-    while (!exited) {
-
-
-    std::cout << "Select by typing the number of the operation:\n";
-
-    std::cout << "1. Load an image to work on.\n";
-    if (fileLoaded) {
-      std::cout << "Filters\n";
-      std::cout << "\t2. Black and White Filter.\n";
-      std::cout << "\t3. Grey Scale Filter.\n";
-      std::cout << "\t4. Invert Filter.\n";
-    }
-    std::cout << "0. Exit.\n";
-    std::cout << "-------------------------------\n";
+  
+  RotateFilter rotate_180(toy1, 180);
+  rotate_180.apply("rotated_180.jpg");
 
 
-    int res;
-    std::cout << "Enter Your Response:\t";
-    std::cin >> res;
-    std::cout << '\n';
+  std::string welcomeMsg = "\nWelcome to the ultimate image processor CPP app.";
+  std::cout << welcomeMsg << "\n";
+  std::cout << std::string(welcomeMsg.length()/5, ' ') << std::string(welcomeMsg.length()*3/5, '=') << std::string(welcomeMsg.length()/5, ' ') << "\n \n";
+
+  bool exited = false;
+  bool fileLoaded = false; // default false
 
 
-    if (res == 1) {
-        Image img = loadImage();
-        fileLoaded = true;
-    } else {
-        exited = true;
-    }
+  while (!exited) {
+  std::cout << "Select by typing the number of the operation:\n";
+
+  std::cout << "1. Load an image to work on.\n";
+  if (fileLoaded) {
+    std::cout << "Filters\n";
+    std::cout << "\t2. Black and White Filter.\n";
+    std::cout << "\t3. Grey Scale Filter.\n";
+    std::cout << "\t4. Invert Filter.\n";
   }
+  std::cout << "0. Exit.\n";
+  std::cout << "-------------------------------\n";
+
+
+  int res;
+  std::cout << "Enter Your Response:\t";
+  std::cin >> res;
+  std::cout << '\n';
+
+
+  if (res == 1) {
+      Image img = loadImage();
+      fileLoaded = true;
+  } else {
+      exited = true;
+  }
+}
 
 
   return 0;
 }
-
