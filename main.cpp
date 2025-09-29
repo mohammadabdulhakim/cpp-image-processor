cc9c5ef21360fb1630fa1a293eb959410795ebd4
Mohammad Abdulhakim <duxwork0@gmail.com>
2025-09-29 17:58:49 +0300

    Add rotateFilter by Hamza

--- main.cpp
+++ main.cpp
@@ -134,6 +134,63 @@ public:
     }
 };
 
+class RotateFilter : public Filter {
+    Image image;
+    int angle;
+public:
+    RotateFilter(Image image, int angle) : image(image), angle(angle) {}
+    void apply() override {
+        try {
+            Image rotated_image;
+
+            switch (angle)
+            {
+                case 90:
+                case 270:
+                    rotated_image = Image(image.height, image.width);
+                break;
+
+                case 180:
+                    rotated_image = Image(image.width, image.height);
+                break;
+            }
+
+
+            double radian = angle * M_PI / 180.0;
+            double cos_Angle = cos(radian);
+            double sin_Angle = sin(radian);
+
+            int cx = image.width / 2;
+            int cy = image.height / 2;
+
+            int ncx = rotated_image.width / 2;
+            int ncy = rotated_image.height / 2;
+
+            for (int x = 0; x < rotated_image.width; x++)
+            {
+                for (int y = 0; y < rotated_image.height; y++)
+                {
+                    int X = cx + (x - ncx) * cos_Angle + (y - ncy) * sin_Angle;
+                    int Y = cy - (x - ncx) * sin_Angle + (y - ncy) * cos_Angle;
+
+                    if (X >= 0 && X < image.width && Y >= 0 && Y < image.height)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            rotated_image(x, y, k) = image(X, Y, k);
+                        }
+                    }
+                }
+            }
+
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            throw;
+        }
+    }
+};
+
 
 
 void loadImage (Image &img) {
